1973471d68ebb172b7b42e4d48f9bcb790cb2820
SCAB <camiloab203@gmail.com>
2025-12-28 09:56:11 -0500

    fix: added classNamwe on componente FadeIn

--- app/components/FadeIn.tsx
+++ app/components/FadeIn.tsx
@@ -1,3 +1,4 @@
+// app/components/FadeIn.tsx
 "use client";
 import { motion } from "framer-motion";
 import { ReactNode } from "react";
@@ -6,23 +7,21 @@ interface Props {
   children: ReactNode;
   delay?: number;
   direction?: "up" | "down" | "left" | "right";
+  className?: string; // Añade esta línea para corregir el error de los logs
 }
 
-export const FadeIn = ({ children, delay = 0, direction }: Props) => {
+export const FadeIn = ({ children, delay = 0, direction, className }: Props) => {
   return (
     <motion.div
+      className={className}
       initial={{ 
         opacity: 0, 
         y: direction === "up" ? 40 : direction === "down" ? -40 : 0,
         x: direction === "left" ? 40 : direction === "right" ? -40 : 0
       }}
       whileInView={{ opacity: 1, y: 0, x: 0 }}
-      viewport={{ once: true, margin: "-100px" }} // Se activa 100px antes de entrar
-      transition={{ 
-        duration: 0.8, 
-        delay: delay, 
-        ease: [0.21, 0.47, 0.32, 0.98] // Ease tipo "premium"
-      }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8, delay }}
     >
       {children}
     </motion.div>
